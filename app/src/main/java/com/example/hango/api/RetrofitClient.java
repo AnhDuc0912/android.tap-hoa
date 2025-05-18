77699632e976d0721c5f1d77b7cb12e48639dc58
haung <haunguyenhd2003@gmail.com>
2025-05-18 11:43:03 +0700

    18/5

--- app/src/main/java/com/example/hango/api/RetrofitClient.java
+++ app/src/main/java/com/example/hango/api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.1.249:5000";
+    private static final String BASE_URL = "http://192.168.1.43:5000";
     private static Retrofit retrofit;
 
     public static Retrofit getInstance() {
@@ -21,4 +21,7 @@ public class RetrofitClient {
         return getInstance().create(ApiService.class);
     }
 
+    public static String getBaseUrl() {
+        return BASE_URL;
+    }
 }
